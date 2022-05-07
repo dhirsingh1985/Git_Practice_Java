017e7b9d894a027fe2668c0b7a791f82a7578c7d
Dhiraj Singh <dhir.singh1985@gmail.com>
2022-05-08 08:28:50 +1000

    Branch3 different from Master for rebase

--- src/GitClass.java
+++ src/GitClass.java
@@ -4,7 +4,7 @@ public class GitClass {
 	public static void main(String[] args) {
 
 
-		System.out.println("Branch 3 SourceTree CONFLICT");
+		System.out.println("Branch 3 changes to different of Master");
 	}
 
 }
